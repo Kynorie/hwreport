84300f0bc1f62e9250629d3d8f56a823f38d7644
Kynorie <py@astrarune.com>
2026-09-13 19:31:28 +0000

    Create version.py for release version management
    Add versioning information for GitHub releases

---
+++ hwreport/version.py
@@ -0,0 +1,4 @@
+# repair and check for updates both read this file
+# when you cut a new github release, bump this and tag it to match
+# example, VERSION = "2" means the release should be tagged v2
+VERSION = "1"
